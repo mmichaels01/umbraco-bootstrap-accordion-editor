31345c07e0bde37195868d2a2a13419ceabf20ae
Mike <mmichaels1010@gmail.com>
2016-06-18 17:44:01 -0500

    Intial commit

---
+++ bs_accordion.controller.js
@@ -0,0 +1,4 @@
+﻿angular.module("umbraco").controller("bs_accordion.controller", function ($scope, notificationsService) {
+    $scope.control.body = $scope.control.body ? $scope.control.body : "Accordion body content";
+    debugger;
+});
